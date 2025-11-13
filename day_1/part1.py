f393a1eb6be4c66ac037bfebc7136821201149a9
Geoffrey Sametz <sametz@users.noreply.github.com>
2025-11-13 10:47:59 -0500

    Day 1 part 1 solved.

---
+++ day_1/part1.py
@@ -0,0 +1,56 @@
+from collections import defaultdict
+
+N = 0 - 1j
+S = 0 + 1j
+E = 1 + 0j
+W = -1 + 0j
+
+moves = {
+    '^': N,
+    'v': S,
+    '<': W,
+    '>': E
+}
+
+left_turn = 0 - 1j
+right_turn = 0 + 1j
+
+
+def read_input():
+    with open('input.txt') as f:
+        steps = f.read().split(", ")
+        return [(step[0], int(step[1:])) for step in steps]
+
+
+# Could either simulate step by step, or count the total number
+# ouf N, S, E, and W steps. I don't think counting number of N/S/E/W
+# steps is more efficient than just adding at each direction,
+# so try the obvious solution.
+
+def follow_directions(steps):
+    pos = 0 + 0j
+    direction = N
+    for turn, d in steps:
+        match turn:
+            case "L":
+                direction *= left_turn
+            case "R":
+                direction *= right_turn
+            case _:
+                raise Exception(f"Illegal move: {turn}{d}")
+        pos += d * direction
+    return pos, direction
+
+
+def manhattan_d(coord):
+    return abs(coord.real) + abs(coord.imag)
+
+
+def main():
+    steps = read_input()
+    pos, dir = follow_directions(steps)
+    return manhattan_d(pos)
+
+
+if __name__ == "__main__":
+    print(f"Bunny HQ is {main()} blocks away.")
---
+++ day_1/test_part1.py
@@ -0,0 +1,38 @@
+import pytest
+
+from part1 import read_input, follow_directions, manhattan_d
+
+
+def test_read_input():
+    result = read_input()
+    assert result[0] == ("L", 3)
+    assert result[-1] == ("L", 1)
+
+
+@pytest.mark.parametrize("steps, expected",
+                         [
+                             ([("R", 2), ("L", 3)], (2 - 3j, 0 - 1j)),
+                             ([
+                                  ("R", 2),
+                                  ("R", 2),
+                                  ("R", 2)
+                              ], (0 + 2j, -1 + 0j)),
+                             ([
+                                  ("R", 5),
+                                  ("L", 5),
+                                  ("R", 5),
+                                  ("R", 3)
+                              ], (10 - 2j, 0 + 1j))
+                         ])
+def test_follow_directions(steps, expected):
+    assert follow_directions(steps) == expected
+
+
+@pytest.mark.parametrize("coord, expected", [
+    (2 - 3j, 5),
+    (0 + 2j, 2),
+    (10 - 2j, 12),
+]
+                         )
+def test_manhattan_d(coord, expected):
+    assert manhattan_d(coord) == expected
