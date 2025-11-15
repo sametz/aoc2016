ccfd9ec76e73dcf844d27a0b9924eb7e2b2cd57d
Geoffrey Sametz <sametz@users.noreply.github.com>
2025-11-14 21:22:22 -0500

    Day 6 part 2 solved.

---
+++ day_6/part2.py
@@ -0,0 +1,23 @@
+from collections import Counter
+
+
+def read_input(filename):
+    with open(filename) as f:
+        messages = f.read().splitlines()
+    return messages
+
+
+def transpose(m):
+    return list(zip(*m))
+
+
+def denoise(m):
+    consensus = ""
+    for column in transpose(m):
+        c = Counter(column)
+        consensus += c.most_common()[-1][0]
+    return consensus
+
+
+if __name__ == "__main__":
+    print(f"message: {denoise(read_input('input.txt'))}")
