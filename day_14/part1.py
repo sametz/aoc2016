0258659452114a6a55e709e841f35228803d2574
sametz <sametz@udel.edu>
2025-12-25 15:48:43 -0600

    Day 14 part 1 solved.

--- day_14/part1.py
+++ day_14/part1.py
@@ -1,21 +1,19 @@
 from collections import Counter, defaultdict, deque
 import hashlib
+import re
+
+
+def find_triplet(s):
+    triplet_regex = re.compile(r"(\w)\1{2}")
+    match = triplet_regex.search(s)
+    if match:
+        return match.group()[0]
+    return None
 
 
 def read_secret_key(filename):
     with open(filename) as f:
         return f.read().strip()
-
-
-def has_triplet(c, s):
-    return c * 3 in s
-
-
-def in_history(c, history):
-    for h in history:
-        if c in h:
-            return True
-    return False
 
 
 def main(key):
@@ -32,22 +30,16 @@ def main(key):
         for m5 in maybe5:
             if m5 * 5 in h:
                 fives[m5].append(i)
-        maybe3 = [k for k, v in c.items() if v > 2]
-        threes = []
-        for m3 in maybe3:
-            if m3 * 3 in h:
-                threes.append(m3)
-        history.append((i, threes))
+        history.append((i, find_triplet(h)))
         if i >= 1000:
             if history[0][1]:
-                three_loc, threes = history[0]
-                for t in threes:
-                    if t in fives:
-                        fives[t] = [s for s in fives[t] if s > three_loc]
-                        if fives[t] and fives[t][0] <= three_loc + 1000:
-                            keys.append(three_loc)
-                            print(
-                                f"i = {three_loc} has three {t} in {hashlib.md5((key + str(three_loc)).encode()).hexdigest()} and five {t} in {hashlib.md5((key + str(fives[t][0])).encode()).hexdigest()}")
+                three_loc, c = history[0]
+                if c in fives:
+                    fives[c] = [s for s in fives[c] if s > three_loc]
+                    if fives[c] and fives[c][0] <= three_loc + 1000:
+                        keys.append(three_loc)
+                        print(
+                            f"i = {three_loc} has three {c} in {hashlib.md5((key + str(three_loc)).encode()).hexdigest()} and five {c} in {hashlib.md5((key + str(fives[c][0])).encode()).hexdigest()}")
         i += 1
     print(keys)
     print(len(keys))
@@ -55,12 +47,5 @@ def main(key):
 
 
 if __name__ == "__main__":
-    key = "abc"
-    n = 18
-    h = hashlib.md5((key + str(n)).encode()).hexdigest()
-    print(h)
-    c = Counter(h)
-    print(c)
-    maybe3 = [k for k, v in c.items() if v > 2]
-    threes = [m for m in maybe3 if m * 3 in h]
-    print(threes)
+    key = read_secret_key("input.txt")
+    print(f"64th key is: {main(key)}")
--- day_14/test_part1.py
+++ day_14/test_part1.py
@@ -1,4 +1,13 @@
-from part1 import main
+from part1 import find_triplet, main
+
+
+# Found reason why test answer was my 68th key not my 64th:
+# "Only consider the first such triplet in a hash"
+# e.g. 534 is 4fc7df57777400b3eeef5350a6eecbdf so it counts the 7 not the 3
+
+
+def test_find_triplet():
+    assert find_triplet("44fc7df57777400b3eeef5350a6eecbdf") == "7"
 
 
 def test_main():
