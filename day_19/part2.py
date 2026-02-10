81e048986152e7bbb804c36f7a9e3d1ba80576d7
Geoffrey Sametz <sametz@users.noreply.github.com>
2026-02-10 12:38:41 -0500

    Day 19 part 2 solved by detecting a pattern in outputs.

--- day_19/part2.py
+++ day_19/part2.py
@@ -1,4 +1,4 @@
-# Algorithm: len(players) // 2 is player to eliminate
+import math
 
 
 def read_input():
@@ -8,12 +8,38 @@ def read_input():
 
 
 def play_game(players):
+    """This is a slow algorithm that closely models a game."""
     while len(players) > 1:
+        if players[0] == min(players):
+            print(f"start: {len(players)} players: \n{players}")
         loser = len(players) // 2
-        players = players[1:loser] + players[loser + 1:] + players[0:1]
+        print(f"dropping: {players[loser]}")
+        players = players[1:loser] + players[loser + 1 :] + players[0:1]
     return players[0]
+
+
+# tested play_game with 100 players to detect pattern
+def find_pattern():
+    results = []
+    for i in range(2, 101):
+        players = list(range(1, i + 1))
+        results.append((i, play_game(players)))
+    for game in results:
+        print(game)
+
+
+def survivor_from_pattern(n):
+    low_power = math.floor(math.log(n, 3))
+    low_n = 3**low_power
+    high_n = 3 ** (low_power + 1)
+
+    if low_n == n:
+        return n
+    if n > low_n and n <= (high_n - low_n):
+        return n - low_n
+    return 2 * n - high_n
 
 
 if __name__ == "__main__":
     secret_number = read_input()
-    print(f"Winner: {play_game(list(range(1, secret_number + 1)))}")
+    print(f"Survivor: {survivor_from_pattern(secret_number)}")
--- day_19/test_part2.py
+++ day_19/test_part2.py
@@ -1,9 +1,22 @@
 import pytest
 
-from part2 import play_game
+from part2 import play_game, survivor_from_pattern
 
 
-@pytest.mark.parametrize("n, expected", [(5, 2), (7, 5),])
+@pytest.mark.parametrize(
+    "n, expected",
+    [
+        (5, 2),
+        (7, 5),
+    ],
+)
 def test_play_game(n, expected):
     players = list(range(1, n + 1))
     assert play_game(players) == expected
+
+
+def test_survivor_from_pattern():
+    for i in range(2, 100):
+        expected = play_game(list(range(1, i + 1)))
+        result = survivor_from_pattern(i)
+        assert result == expected
